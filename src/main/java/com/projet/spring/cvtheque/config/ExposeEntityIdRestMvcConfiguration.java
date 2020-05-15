bcb0b4874f76cafd273abd26a496947e62b69c95
Bertrand Verdun <bertrand.verdun@gmail.com>
2020-05-15 10:06:24 +0200

    Adding config file to enable profileId in the JSON returned on /profiles and /profile/{id} requests.

---
+++ src/main/java/com/projet/spring/cvtheque/config/ExposeEntityIdRestMvcConfiguration.java
@@ -0,0 +1,15 @@
+package com.projet.spring.cvtheque.config;
+
+import com.projet.spring.cvtheque.entity.Profile;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExposeEntityIdRestMvcConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Profile.class);
+    }
+}
--- src/main/java/com/projet/spring/cvtheque/dao/ProfileRepository.java
+++ src/main/java/com/projet/spring/cvtheque/dao/ProfileRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import com.projet.spring.cvtheque.entity.Profile;
 
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(/*"http://localhost:4200"*/)
 public interface ProfileRepository extends JpaRepository<Profile,Integer> {
 
 }
